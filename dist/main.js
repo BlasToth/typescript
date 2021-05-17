41fb6ae1a53626fcf9af7f29914c4db98421eb3e
BlasToth <balazstothlaszlo+github@gmail.com>
2021-05-17 10:29:15 +0200

    first steps TypeScript

---
+++ dist/main.js
@@ -0,0 +1,23 @@
+var hello = "foo";
+hello = "World";
+console.log("Hello TS " + hello);
+var getFullName = function (name, surname) {
+    return name + " " + surname;
+};
+console.log(getFullName("Type", "Script"));
+// types
+var myString = "Hello world";
+var user = {
+    name: "Pepito",
+    age: 23,
+    getMessage: function () {
+        return "Hello " + user.name;
+    }
+};
+var user2 = {
+    name: "Grillo",
+    getMessage: function () {
+        return "Hello " + user2.name;
+    }
+};
+console.log(user.getMessage());
---
+++ src/main.ts
@@ -0,0 +1,34 @@
+let hello: string = "foo";
+hello = "World";
+console.log("Hello TS " + hello);
+
+const getFullName = (name: string, surname: string): string => {
+    return name + " " + surname;
+}
+console.log(getFullName("Type", "Script"))
+// types
+
+var myString: string = "Hello world";
+//myString = 22; //Type 'number' is not assignable to type 'string'.ts(2322)
+
+// interfaces
+interface UserInterFace {
+    name: string;
+    age?: number;
+    getMessage(): string;
+}
+const user: UserInterFace = {
+    name: "Pepito",
+    age: 23,
+    getMessage() {
+        return "Hello " + user.name;
+    }
+}
+const user2: UserInterFace = {
+    name: "Grillo",
+    getMessage() {
+        return "Hello " + user2.name;
+    }
+
+};
+console.log(user.getMessage())
